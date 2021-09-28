fcd1174be7c00be54160131ec0a98343d55f0c61
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2021-09-28 21:29:27 +0400

    if else moment

--- main.exs
+++ main.exs
@@ -4,9 +4,13 @@ defmodule Testcode do
         # Taking the user input and spliting it
         percentage = fn (x, y) -> (x*(String.to_integer(marks)))/y end
         # defining func percentage, where we convert string number to int
-        IO.puts "Percentage is: #{percentage.(100, 500)}"
+
+        if String.to_integer(marks) > 500 do #messing around with conditional statements
+            IO.puts "Enter number less than or equal to 500"
+        else
+            IO.puts "Percentage is: #{percentage.(100, 500)}"
+        end
         # outputing the percentage and setting param x and y
     end
 end
 Testcode.main() # running the module
-
